afbfd4946ad151b068bdff63a5afb684759aaa2d
Cooper Sawyer <cooper.sawyer@powerhrg.com>
2021-06-07 11:00:59 -0400

    eventlistener for button

--- src/index.js
+++ src/index.js
@@ -1,15 +1,47 @@
-let addToy = false;
+let addToy = false
+const toyContainer = document.querySelector('#toy-collection')
 
-document.addEventListener("DOMContentLoaded", () => {
-  const addBtn = document.querySelector("#new-toy-btn");
-  const toyFormContainer = document.querySelector(".container");
-  addBtn.addEventListener("click", () => {
+const createNewCard = (toy) => {
+  const cardsClass = document.createElement('div')
+  cardsClass.classList.add('card')
+  const h2 = document.createElement('h2')
+  h2.innerText = toy.name
+  const img = document.createElement('img')
+  img.src = toy.image
+  img.classList.add('toy-avatar')
+  const p = document.createElement('p')
+  p.innerText = `${toy.likes} likes`
+  const btn = document.createElement('button')
+  btn.classList.add('like-btn')
+  btn.setAttribute('id', toy.id)
+  btn.innerText = 'Like'
+  cardsClass.append(h2, p, img, btn)
+  toyContainer.append(cardsClass)
+}
+
+document.addEventListener('DOMContentLoaded', () => {
+  const addBtn = document.querySelector('#new-toy-btn')
+  const toyFormContainer = document.querySelector('.container')
+
+  fetch('http://localhost:3000/toys')
+    .then(resp => resp.json())
+    .then(data => {
+      data.forEach(toy => createNewCard(toy))
+    })
+    
+    const submitToyButton = document.querySelector('.submit')
+    submitToyButton.addEventListener('click', (event) => {  
+      event.preventDefault()
+ 
+    })
+
+  addBtn.addEventListener('click', () => {
     // hide & seek with the form
-    addToy = !addToy;
+    addToy = !addToy
     if (addToy) {
-      toyFormContainer.style.display = "block";
+      toyFormContainer.style.display = 'block'
     } else {
-      toyFormContainer.style.display = "none";
+      toyFormContainer.style.display = 'none'
     }
-  });
-});
+  })
+})
